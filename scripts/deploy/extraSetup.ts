5b961a0ac7f0d3704ac4abb2985f9c8e15c7caa7
web3sdkio <demo@web3sdks.com>
2022-11-17 15:05:42 +0800

    Add extra contracts

---
+++ scripts/deploy/extraSetup.ts
@@ -0,0 +1,82 @@
+import hre, { ethers } from "hardhat";
+
+import {
+  Forwarder,
+  ForwarderEOAOnly,
+  BiconomyForwarder,
+  TWRegistry,
+} from "typechain";
+
+async function verify(address: string, args: any[]) {
+  try {
+    let options = {
+      address: address,
+      constructorArguments: args,
+    };
+    if (contract) {
+      options["contract"] = contract;
+    }
+    return await hre.run("verify:verify", options);
+  } catch (e) {
+    console.log(address, args, e);
+  }
+}
+
+async function main() {
+  // Deploy FeeType
+  const options = {
+    //maxFeePerGas: ethers.utils.parseUnits("50", "gwei"),
+    //maxPriorityFeePerGas: ethers.utils.parseUnits("50", "gwei"),
+    //gasPrice: ethers.utils.parseUnits("100", "gwei"),
+    gasLimit: 5_000_000,
+  };
+
+  const [deployer]: SignerWithAddress[] = await ethers.getSigners();
+  console.log("Deployer address:", deployer.address);
+
+  // Deploy ForwarderEOAOnly
+  const forwarderEOAOnly = await (
+    await ethers.getContractFactory("ForwarderEOAOnly")
+  ).deploy(options);
+  // const forwarderEOAOnly = await ethers.getContractAt("ForwarderEOAOnly", "0xBF2cd070B6Ce0e7cEcf4619be89B2093afB2B13b");
+  console.log("Deploying ForwarderEOAOnly at tx: ", forwarderEOAOnly.deployTransaction?.hash);
+  await forwarderEOAOnly.deployed();
+  console.log("ForwarderEOAOnly address: ", forwarderEOAOnly.address);
+
+  // Deploy BiconomyForwarder
+  const biconomyForwarder = await (
+    await ethers.getContractFactory("BiconomyForwarder")
+  ).deploy(deployer.address, options);
+  // const biconomyForwarder = await ethers.getContractAt("BiconomyForwarder", "0x880B4e2b2eD6c68EA30125989F1560264cE737Df");
+  console.log("Deploying BiconomyForwarder at tx: ", biconomyForwarder.deployTransaction?.hash);
+  await biconomyForwarder.deployed();
+  console.log("BiconomyForwarder address: ", biconomyForwarder.address);
+
+  // Get Forwarder
+  const trustedForwarderAddress: string = "0x5D1021751de591fEd8562eE92731F6D612D4cC81"; // replace
+  const trustedForwarder: Forwarder = await ethers.getContractAt("Forwarder", trustedForwarderAddress);
+  await trustedForwarder.deployed();
+  console.log("Trusted Forwarder address: ", trustedForwarder.address);
+
+  // Deploy TWBYOCRegistry
+  const twBYOCRegistry = await (
+    await ethers.getContractFactory("TWRegistry")
+  ).deploy(trustedForwarderAddress, options);
+  // const twBYOCRegistry = await ethers.getContractAt("TWRegistry", "0x1A083bCB5Fe719275329C80d7D033DCaCC21461a");
+  console.log("Deploying TWBYOCRegistry at tx: ", twBYOCRegistry.deployTransaction?.hash);
+  await twBYOCRegistry.deployed();
+  console.log("TWBYOCRegistry address: ", twBYOCRegistry.address);
+
+  // Verify contracts
+  console.log("DONE. Now verifying contracts...");
+  await verify(forwarderEOAOnly.address, [], "contracts/forwarder/ForwarderEOAOnly.sol:ForwarderEOAOnly");
+  await verify(biconomyForwarder.address, [deployer.address]);
+  await verify(twBYOCRegistry.address, [trustedForwarderAddress]);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(e => {
+    console.error(e);
+    process.exit(1);
+  });
