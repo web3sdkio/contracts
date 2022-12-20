a5d019c5c3b5f53610e735d6759ae14130f65464
web3sdkio <demo@web3sdks.com>
2022-12-20 09:55:29 +0000

    Modify fullV2Setup.ts

--- scripts/deploy/fullV2Setup.ts
+++ scripts/deploy/fullV2Setup.ts
@@ -40,7 +40,7 @@ async function main() {
   };
 
   const trustedForwarder = await (await ethers.getContractFactory("Forwarder")).deploy(options);
-  // const trustedForwarder = await ethers.getContractAt("Forwarder", "0x5D1021751de591fEd8562eE92731F6D612D4cC81");
+  // const trustedForwarder = await ethers.getContractAt("Forwarder", "0x5D1021751de591fEd8562eE92731F6D612D4cC81"); // goerli
   console.log("Deploying Trusted Forwarder at tx: ", trustedForwarder.deployTransaction?.hash);
   await trustedForwarder.deployed();
   console.log("Trusted Forwarder address: ", trustedForwarder.address);
@@ -50,7 +50,7 @@ async function main() {
   const web3sdkioRegistry = await (
     await ethers.getContractFactory("TWRegistry")
   ).deploy(trustedForwarderAddress, options);
-  // const web3sdkioRegistry = await ethers.getContractAt("TWRegistry", "0xB161f6EA00A3008F20a32d465E5364B0905A33Ad");
+  // const web3sdkioRegistry = await ethers.getContractAt("TWRegistry", "0xB161f6EA00A3008F20a32d465E5364B0905A33Ad"); // goerli
   console.log("Deploying TWRegistry at tx: ", web3sdkioRegistry.deployTransaction?.hash);
   await web3sdkioRegistry.deployed();
   console.log("TWRegistry address: ", web3sdkioRegistry.address);
@@ -59,7 +59,7 @@ async function main() {
   const web3sdkioFactory = await (
     await ethers.getContractFactory("TWFactory")
   ).deploy(trustedForwarderAddress, web3sdkioRegistry.address, options);
-  // const web3sdkioFactory = await ethers.getContractAt("TWFactory", "0x80a1d6255191D2Af220a35640F393de5B6266f02");
+  // const web3sdkioFactory = await ethers.getContractAt("TWFactory", "0x80a1d6255191D2Af220a35640F393de5B6266f02"); // goerli
   console.log("Deploying TWFactory at tx: ", web3sdkioFactory.deployTransaction?.hash);
   await web3sdkioFactory.deployed();
   console.log("TWFactory address: ", web3sdkioFactory.address);
@@ -68,7 +68,7 @@ async function main() {
   const web3sdkioFee: TWFee = await ethers
     .getContractFactory("TWFee")
     .then(f => f.deploy(trustedForwarderAddress, web3sdkioFactory.address, options));
-  // const web3sdkioFee = await ethers.getContractAt("TWFee", "0x2d484d217F3CF9C0AF4E4bE27825B946B0F1049F");
+   const web3sdkioFee = await ethers.getContractAt("TWFee", "0x2d484d217F3CF9C0AF4E4bE27825B946B0F1049F"); // goerli
   console.log("Deploying TWFee at tx: ", web3sdkioFee.deployTransaction?.hash);
   await web3sdkioFee.deployed();
   console.log("TWFee address: ", web3sdkioFee.address);
@@ -78,7 +78,7 @@ async function main() {
     .getContractFactory("DropERC721")
     .then(f => f.deploy(options))
     .then(f => f.deployed());
-  // const drop721 = await ethers.getContractAt("DropERC721", "0x20e099109dbEbBD11058EeB39417B2eCA52C83f0");
+  // const drop721 = await ethers.getContractAt("DropERC721", "0x20e099109dbEbBD11058EeB39417B2eCA52C83f0"); // goerli
   console.log("Deploying DropERC721 at tx: ", drop721.deployTransaction?.hash);
   console.log("DropERC721 address: ", drop721.address);
 
@@ -89,7 +89,7 @@ async function main() {
     .then(f => f.deployed());
   console.log("Deploying Drop1155 at tx: ", drop1155.deployTransaction.hash);
   console.log("Drop1155 address: ", drop1155.address);
-  // const drop1155 = await ethers.getContractAt("DropERC1155", "0xEE89E8E54271F30738883e28DD7f8226805a6848");
+  // const drop1155 = await ethers.getContractAt("DropERC1155", "0xEE89E8E54271F30738883e28DD7f8226805a6848"); // goerli
 
   // Deploy a test implementation: DropERC20
   const drop20: DropERC20 = await ethers
@@ -98,7 +98,7 @@ async function main() {
     .then(f => f.deployed());
   console.log("Deploying DropERC20 at tx: ", drop20.deployTransaction.hash);
   console.log("DropERC20 address: ", drop20.address);
-  // const drop20 = await ethers.getContractAt("DropERC20", "0x5Abe68be6ecE6586Bd438BE25cEed2D5707F2a95");
+  // const drop20 = await ethers.getContractAt("DropERC20", "0x5Abe68be6ecE6586Bd438BE25cEed2D5707F2a95"); // goerli
 
   // Deploy a test implementation: TokenERC20
   const tokenERC20: TokenERC20 = await ethers
@@ -107,7 +107,7 @@ async function main() {
     .then(f => f.deployed());
   console.log("Deploying TokenERC20 at tx: ", tokenERC20.deployTransaction.hash);
   console.log("TokenERC20 address: ", tokenERC20.address);
-  // const tokenERC20 = await ethers.getContractAt("TokenERC20", "0x07Fd3DAdf1f81BfA0ab0841c33e44789E78335F0");
+  // const tokenERC20 = await ethers.getContractAt("TokenERC20", "0x07Fd3DAdf1f81BfA0ab0841c33e44789E78335F0"); // goerli
 
   // Deploy a test implementation: TokenERC721
   const tokenERC721: TokenERC721 = await ethers
@@ -116,7 +116,7 @@ async function main() {
     .then(f => f.deployed());
   console.log("Deploying TokenERC721 at tx: ", tokenERC721.deployTransaction.hash);
   console.log("TokenERC721 address: ", tokenERC721.address);
-  // const tokenERC721 = await ethers.getContractAt("TokenERC721", "0x2F7f4Ac01119F8F99B94F0927EfA27D7818A074b");
+  // const tokenERC721 = await ethers.getContractAt("TokenERC721", "0x2F7f4Ac01119F8F99B94F0927EfA27D7818A074b"); // goerli
 
   // Deploy a test implementation: TokenERC1155
   const tokenERC1155: TokenERC1155 = await ethers
@@ -125,7 +125,7 @@ async function main() {
     .then(f => f.deployed());
   console.log("Deploying TokenERC1155 at tx: ", tokenERC1155.deployTransaction.hash);
   console.log("TokenERC1155 address: ", tokenERC1155.address);
-  // const tokenERC1155 = await ethers.getContractAt("TokenERC1155", "0x436bC195D4BFFD9aA0DfB20688737cF7D7Ab3e35");
+  // const tokenERC1155 = await ethers.getContractAt("TokenERC1155", "0x436bC195D4BFFD9aA0DfB20688737cF7D7Ab3e35"); // goerli
 
   const split: Split = await ethers
     .getContractFactory("Split")
@@ -133,7 +133,7 @@ async function main() {
     .then(f => f.deployed());
   console.log("Deploying Split at tx: ", split.deployTransaction.hash);
   console.log("Split address: ", split.address);
-  // const split = await ethers.getContractAt("Split", "0xC5a73A3794423CB30029ab5c6d0acb22f6CcCDA2");
+  // const split = await ethers.getContractAt("Split", "0xC5a73A3794423CB30029ab5c6d0acb22f6CcCDA2"); // goerli
 
   const marketplace: Marketplace = await ethers
     .getContractFactory("Marketplace")
@@ -141,7 +141,7 @@ async function main() {
     .then(f => f.deployed());
   console.log("Deploying Marketplace at tx: ", marketplace.deployTransaction.hash);
   console.log("Marketplace address: ", marketplace.address);
-  // const marketplace = await ethers.getContractAt("Marketplace", "0x28F97d65F66F5D4f152e9A46Cf3D6DF22307e86d");
+  // const marketplace = await ethers.getContractAt("Marketplace", "0x28F97d65F66F5D4f152e9A46Cf3D6DF22307e86d"); // goerli
 
   const vote: VoteERC20 = await ethers
     .getContractFactory("VoteERC20")
@@ -149,7 +149,7 @@ async function main() {
     .then(f => f.deployed());
   console.log("Deploying vote at tx: ", vote.deployTransaction.hash);
   console.log("Vote address: ", vote.address);
-  // const vote = await ethers.getContractAt("VoteERC20", "0x73ab01d8Eeb0Ac0C012BC1ca20d5A0494C38a376");
+  // const vote = await ethers.getContractAt("VoteERC20", "0x73ab01d8Eeb0Ac0C012BC1ca20d5A0494C38a376"); // goerli
 
   // Multiwrap
   const multiwrap: Multiwrap = await ethers
@@ -158,7 +158,7 @@ async function main() {
     .then(f => f.deployed());
   console.log("Deploying Multiwrap at tx: ", multiwrap.deployTransaction.hash);
   console.log("Multiwrap address: ", multiwrap.address);
-  // const multiwrap = await ethers.getContractAt("Multiwrap", "0x831e2B04c4aF9a3759C5D8FeA49692e8a0294B01");
+  // const multiwrap = await ethers.getContractAt("Multiwrap", "0x831e2B04c4aF9a3759C5D8FeA49692e8a0294B01"); // goerli
 
   // Signature Drop
   const sigdrop: SignatureDrop = await ethers
@@ -167,7 +167,7 @@ async function main() {
     .then(f => f.deployed());
   console.log("Deploying SignatureDrop at tx: ", sigdrop.deployTransaction.hash);
   console.log("SignatureDrop address: ", sigdrop.address);
-  // const sigdrop = await ethers.getContractAt("SignatureDrop", "0xFDb9cc34BfA6a51e96F73f0a828717A6dfA460E8");
+  // const sigdrop = await ethers.getContractAt("SignatureDrop", "0xFDb9cc34BfA6a51e96F73f0a828717A6dfA460E8"); // goerli
 
   // AirdropERC20
   const airdropERC20: AirdropERC20 = await ethers
@@ -176,7 +176,7 @@ async function main() {
     .then(f => f.deployed());
   console.log("Deploying AirdropERC20 at tx: ", airdropERC20.deployTransaction.hash);
   console.log("AirdropERC20 address: ", airdropERC20.address);
-  // const airdropERC20 = await ethers.getContractAt("AirdropERC20", "0xFDb9cc34BfA6a51e96F73f0a828717A6dfA460E8");
+  // const airdropERC20 = await ethers.getContractAt("AirdropERC20", "0x1EAdBab5149aB0Ed6D0f7838915FD7A3eE2148c5"); // goerli
 
   // AirdropERC721
   const airdropERC721: AirdropERC721 = await ethers
@@ -185,7 +185,7 @@ async function main() {
     .then(f => f.deployed());
   console.log("Deploying AirdropERC721 at tx: ", airdropERC721.deployTransaction.hash);
   console.log("AirdropERC721 address: ", airdropERC721.address);
-  // const airdropERC721 = await ethers.getContractAt("AirdropERC721", "0xFDb9cc34BfA6a51e96F73f0a828717A6dfA460E8");
+  // const airdropERC721 = await ethers.getContractAt("AirdropERC721", "0xC38955FF2949B92c38d576258b6C6A31d23ea0c1"); // goerli
 
   // AirdropERC1155
   const airdropERC1155: AirdropERC1155 = await ethers
@@ -194,7 +194,7 @@ async function main() {
     .then(f => f.deployed());
   console.log("Deploying AirdropERC1155 at tx: ", airdropERC1155.deployTransaction.hash);
   console.log("AirdropERC1155 address: ", airdropERC1155.address);
-  // const airdropERC1155 = await ethers.getContractAt("AirdropERC1155", "0xFDb9cc34BfA6a51e96F73f0a828717A6dfA460E8");
+  // const airdropERC1155 = await ethers.getContractAt("AirdropERC1155", "0xDBD78313131892b73aecB9C4aE89bD3502e3c3bD"); // goerli
 
   // TODO Pack
 
